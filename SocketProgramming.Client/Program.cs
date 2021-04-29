2006c1f8bb86a8e90a3fd9c1e3edf28fb4892123
Henry Beck <terminator0123456@yahoo.com>
2021-04-30 06:28:30 +0700

    init project Client

---
+++ SocketProgramming.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketProgramming.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
